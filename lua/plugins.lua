5dcfe0f860e92c2b395b2b807dd07897db4d19c5
lilifaj <nicolas.descouens@live.com>
2023-03-07 11:27:32 +0900

    removed the custom color plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,13 +21,8 @@ packer.startup(function()
     use 'ryanoasis/vim-devicons'
     use 'ellisonleao/glow.nvim'
     use 'mechatroner/rainbow_csv'
-    use {'neoclide/coc.nvim', branch = 'release'}
   end
 )
-vim.g.neon_style = "doom"
-vim.g.neon_italic_keyword = true
-vim.g.neon_italic_function = true
-vim.cmd[[colorscheme neon]]
 
 require('hardline').setup {}
 require("nvim-tree").setup({
