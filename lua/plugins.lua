9dbe1f878233360076340efb7aa45c47f59154c2
Nicolas Descouens <nicolas.descouens@live.fr>
2024-07-29 13:10:35 +0900

    adapt nvim tree installation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,8 +14,7 @@ packer.startup(function()
   'nvim-tree/nvim-tree.lua',
   requires = {
     'nvim-tree/nvim-web-devicons', -- optional, for file icons
-  },
-  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 }
   use 'ojroques/nvim-hardline'
   use 'ryanoasis/vim-devicons'
@@ -50,16 +49,11 @@ require'nvim-treesitter.configs'.setup {
 }
 
 require('hardline').setup {}
+
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-        { key = "C-n", action = "open" }
-      },
-    },
   },
   renderer = {
     group_empty = true,
